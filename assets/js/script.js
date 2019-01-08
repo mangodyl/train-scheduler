5091c49fdbca76619341f6e58a4fc44a493a3e6d
mango dyl <dylanbonds@hotmail.com>
2019-01-08 14:41:43 -0600

    links set up

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC6OcB6cZ3_VMNZcS24sxf1zHUBktnbmDo",
+    authDomain: "train-scheduler-demo.firebaseapp.com",
+    databaseURL: "https://train-scheduler-demo.firebaseio.com",
+    projectId: "train-scheduler-demo",
+    storageBucket: "train-scheduler-demo.appspot.com",
+    messagingSenderId: "1034003993438"
+  };
+  firebase.initializeApp(config);
+
+  
