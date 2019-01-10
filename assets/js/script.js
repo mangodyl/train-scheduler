48cf46c321b553560a3c9333fb6574e699d5962d
mango dyl <dylanbonds@hotmail.com>
2019-01-09 18:19:40 -0600

    js on click almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,51 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyC6OcB6cZ3_VMNZcS24sxf1zHUBktnbmDo",
-    authDomain: "train-scheduler-demo.firebaseapp.com",
-    databaseURL: "https://train-scheduler-demo.firebaseio.com",
-    projectId: "train-scheduler-demo",
-    storageBucket: "train-scheduler-demo.appspot.com",
-    messagingSenderId: "1034003993438"
+  apiKey: "AIzaSyC6OcB6cZ3_VMNZcS24sxf1zHUBktnbmDo",
+  authDomain: "train-scheduler-demo.firebaseapp.com",
+  databaseURL: "https://train-scheduler-demo.firebaseio.com",
+  projectId: "train-scheduler-demo",
+  storageBucket: "train-scheduler-demo.appspot.com",
+  messagingSenderId: "1034003993438"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot) {
+
+  console.log(snapshot);
+
+});
+
+$(document).on("click", '#submit', function (event) {
+  
+  event.preventDefault();
+
+  console.log("ok");
+
+  var newTrain = $("#new-train").val().trim();
+  var newDest = $("#new-dest").val().trim();
+  var newTime = $("#new-time").val().trim();
+  var newFreq = parseInt($("#new-freq").val().trim());
+
+  function newTrainFunc (insTrain, insDest, insTime, insFreq) {
+    database.ref().push({
+
+      trainName: insTrain,
+      destination: insDest,
+      time: insTime,
+      frequency: insFreq,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+    });
   };
-  firebase.initializeApp(config);
 
-  var database = firebase.database();
+  newTrainFunc (newTrain, newDest, newTime, newFreq);
 
-  database.ref().on("value", function (snapshot) {
+  $("#new-train").val("");
+  $("#new-dest").val("");
+  $("#new-time").val("");
+  $("#new-freq").val("");
 
-    console.log(snapshot);
-
-  });
-
-$("#submit").click(function (event) {
-    event.preventDefault();
-
-    var newTrain = $("#new-train").val().trim();
-    var newDest = $("#new-dest").val().trim();
-    var newTime = parseInt($("#new-time").val().trim());
-    var newFreq = parseInt($("#new-freq").val().trim());
-
-    database.ref();
 
 });
